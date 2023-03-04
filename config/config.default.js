68b91e31c9ec476fcf17fd66fe854bc2bb441892
飞蛛 <669483446@qq.com>
2023-03-04 17:09:02 +0800

    【feat】连接数据库，解决跨域问题

--- config/config.default.js
+++ config/config.default.js
@@ -22,8 +22,32 @@ module.exports = appInfo => {
   const userConfig = {
     // myAppName: 'egg',
   };
+  const mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: 'localhost',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: '669483',
+      // 数据库名
+      database: 'lifeline_community',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+  const security = {
+    domainWhiteList: [ 'http://localhost:5174', 'http://localhost:5173' ],
+  };
   return {
     ...config,
     ...userConfig,
+    mysql,
+    security,
   };
 };
---
+++ config/plugin.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+module.exports = {
+  // had enabled by egg
+  // static: {
+  //   enable: true,
+  // }
+  // config/plugin.js
+  mysql: {
+    enable: true,
+    package: 'egg-mysql',
+  },
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
+};
